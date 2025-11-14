5ca3d4f75429a8bffa37e297730fbe1f0703260e
J. Lê <jle_m1@Js-MacBook-Pro.local>
2025-11-14 14:54:06 -0800

    Edited package.json to utilize import, generated certificate for local dev, and added try catch to routes

--- server/http2.js
+++ server/http2.js
@@ -4,8 +4,8 @@ import fetch from "node-fetch";
 
 // create HTTP2 secure server
 const server = http2.createSecureServer({
-  key: fs.readFileSync("./cert/localhost.key"),
-  cert: fs.readFileSync("./cert/localhost.cert"),
+  key: fs.readFileSync("./localhost-key.pem"),
+  cert: fs.readFileSync("./localhost.pem"),
 });
 
 // Handle incoming streams HTTP2 doesn't use old-school "req" and "res"
@@ -16,21 +16,27 @@ server.on("stream", async (stream, headers) => {
   const method = headers[":method"];
 
   if (path === "/api/products" && method === "GET") {
-    const response = await fetch("https://fakestoreapi.com/products");
-    const data = await response.json();
+    try {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+
+      stream.respond({
+        "content-type": "application/json",
+        ":status": 200,
+        "x-protocol": "http2"
+      });
+
+      return stream.end(JSON.stringify({ protocol: "http2", data }));
+    } catch (err) {
+      stream.respond({ ":status": 500 });
+      return stream.end("Error fetching products");
+    }
   }
 
-  stream.respond({
-    "content-type": "application/json",
-    ":status": 200,
-    "x-protocol": "http2"
-  });
-
-  return stream.end(JSON.stringify({ protocol: "http2", data }));
-
-  // serve frontend 
+  // serve frontend: GET
   if (path === "/") {
     const html = fs.readFileSync("./public/index.html");
+
     stream.respond({
       "content-type": "text/html",
       ":status": 200
@@ -40,14 +46,22 @@ server.on("stream", async (stream, headers) => {
 
   // serve static files and CSS file
   if (path.endsWith(".js") || path.endsWith(".css")) {
-    const file = fs.readFileSync(`./public${path}`);
-    const type = path.endsWith(".js") ? "application/javascript" : "text/css";
-    stream.respond({
-      "content-type": type,
-      ":status": 200
-    });
+    try {
+      const file = fs.readFileSync(`./public${path}`);
+      const type = path.endsWith(".js") ? "application/javascript" : "text/css";
 
-    return stream.end(file);
+      stream.respond({
+        "content-type": type,
+        ":status": 200
+      });
+
+      return stream.end(file);
+
+
+    } catch {
+      stream.respond({ ":status": 404 });
+      return stream.end("File not found");
+    }
   }
 
   // fallback: return error feedback
