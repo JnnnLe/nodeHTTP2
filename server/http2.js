772225e677b38fd9be49b493ea6b651f7eb473a4
J. Lê <jle_m1@Js-MacBook-Pro.local>
2025-11-14 13:50:06 -0800

    Created secure server, custom header, for /products path

--- server/http2.js
+++ server/http2.js
@@ -1,3 +1,35 @@
 import fs from "fs";
 import http2 from "http2";
 import fetch from "node-fetch";
+
+// create HTTP2 secure server
+const server = http2.createSecureServer({
+  key: fs.readFileSync("./cert/localhost.key"),
+  cert: fs.readFileSync("./cert/localhost.cert"),
+});
+
+// Handle incoming streams HTTP2 doesn't use old-school "req" and "res"
+server.on("stream", async (stream, headers) => {
+
+  // custom header
+  const path = headers[":path"];
+  const method = headers[":method"];
+
+  if (path === "/api/products" && method === "GET") {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = await response.json();
+  }
+
+  stream.respond({
+    "content-type": "application/json",
+    ":status": 200,
+    "x-protocol": "http2"
+  });
+
+  return stream.end(JSON.stringify({ protocol: "http2", data }));
+});
+
+// server listen for connecttion
+server.listen(8443, () => {
+  console.log("HTTP/2 server running https://localhost:8443");
+})
